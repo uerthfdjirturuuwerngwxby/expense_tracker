ff8e05fa05370872709418a5e694100dd2f98e73
uerthfdjirturuuwerngwxby <samalanithin18@gmail.com>
2026-04-23 20:27:33 +0530

    changed

--- ml_service/app.py
+++ ml_service/app.py
@@ -683,7 +683,7 @@ def initialize() -> None:
 initialize()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", "5000")), debug=False)
 
 
 
